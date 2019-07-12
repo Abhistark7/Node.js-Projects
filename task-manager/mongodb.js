7acc46c51eb1845c189d31923618fade3bf81070
Abhistark7 <abhistark7@gmail.com>
2019-07-13 00:03:48 +0530

    Added task-manager app

---
+++ task-manager/mongodb.js
@@ -0,0 +1,48 @@
+// CRUD create read update delete
+
+const { MongoClient, ObjectId } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+const id = new ObjectId()
+console.log(id.id.length)
+console.log(id.getTimestamp())
+console.log(id.toHexString().length)
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Error connecting to mongodb!')
+    }
+
+    const db = client.db(databaseName)
+
+    // db.collection('users').updateOne({
+    //     _id: new ObjectId('5d28aded8bd5880ba155987d')
+    // }, {
+    //     $inc: {
+    //         age: 3
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((result) => {
+    //     console.log(result)
+    // })
+
+    // db.collection('tasks').updateMany({ completed: true }, {
+    //     $set: {
+    //         completed: false
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((result) => {
+    //     console.log(result)
+    // })
+
+    db.collection('tasks').deleteOne({ completed: false }).then((result) => {
+        console.log('Deleted succesfully')
+    }).catch((result) => {
+        console.log('Cannot delete!')
+    })
+
+})
