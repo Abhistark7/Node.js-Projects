7d240bc31d821e7f99098412a7c558b16ef04074
Abhistark7 <abhistark7@gmail.com>
2019-07-31 08:53:03 +0530

    Added chat app

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,19 +1,24 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 
 const port = process.env.PORT || 3000
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 app.use(express.json())
 
 // Setup static directory
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-app.get('/', (req, res) => {
-    res.render('index')
+io.on('connection', () => {
+    console.log('New Socket connection')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is up on port ', port)
 })
